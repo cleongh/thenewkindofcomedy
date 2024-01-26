827e4097825cda3dc347663c441ea0aedddaf713
Carlos León <cleon@ucm.es>
2024-01-26 20:28:56 +0100

    inicio de tiled

--- src/level1.js
+++ src/level1.js
@@ -1,6 +1,7 @@
 import Phaser from 'phaser'
 import Level from './level.js'
 
+import map from '../assets/maps/level1.json'
 
 export default class Level1 extends Level {
     /**
@@ -10,12 +11,20 @@ export default class Level1 extends Level {
         super('level1', 50, 50);
     }
 
+    preload() {
+        this.load.tilemapTiledJSON('tilemap', map)
+    }
+
     /**
      * Creación de los elementos de la escena principal de juego
      */
     create() {
         super.create()
         this.add.text(10, 10, "nivel 1")
+        this.map = this.make.tilemap({ key: 'tilemap' })
+        
+        const conId1 = this.map.createFromObjects('objetos', {class: "persona"})
+        console.log(conId1)
         
     }
 
