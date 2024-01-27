0842990e23f8910eee190bd8ba51a4946b0d7938
Pablo Gutiérrez Sánchez <pabgut02@ucm.es>
2024-01-27 17:28:42 +0100

    Docs Bebé

--- src/puzzles/babyTalkLists.ts
+++ src/puzzles/babyTalkLists.ts
@@ -10,4 +10,17 @@ export const babyTalkManualCombinations: BabyTalkManualCombination[] = [
   { syllables: ["Gui", "Pi", "Bi"], correctAction: "Dale Juguetes" },
   { syllables: ["Mu", "Pu", "Pu"], correctAction: "Llévale al Baño" },
   { syllables: ["Mu", "Be", "Be"], correctAction: "Ponle el Chupete" },
+
+  {
+    syllables: ["Gu", "Ga", "Gu", "Ga", "Gu", "Ga"],
+    correctAction: "Sácale los Gases",
+  },
+  { syllables: ["Ga"], correctAction: "Dale de Comer" },
+  { syllables: ["Me", "Mu", "Be", "Bi"], correctAction: "Échale a Dormir" },
+  { syllables: ["Pu", "Pu", "Be", "Ga"], correctAction: "Dale Juguetes" },
+  {
+    syllables: ["Me", "Pi", "Pi", "Pi", "Pi"],
+    correctAction: "Llévale al Baño",
+  },
+  { syllables: ["Gui", "Gui", "Be", "Be"], correctAction: "Ponle el Chupete" },
 ];
